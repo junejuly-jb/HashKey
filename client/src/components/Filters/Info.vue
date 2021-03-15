ef7a7520700e9c9c1dd7e512554d2732f432beb5
june <junearagoncillo@gmail.com>
2021-03-15 09:53:10 +0800

    🧑 personal info

--- client/src/components/Filters/Info.vue
+++ client/src/components/Filters/Info.vue
@@ -1,12 +1,27 @@
 <script>
+import UniversalDialog from '../Main/UniversalDialog'
 export default {
+    components: { UniversalDialog },
     data: () => ({
-
-    })
+        dialogStat: false,
+        header: 'Add Personal Information',
+        type: 'info'
+    }),
+    methods: {
+        onDialogShow(){
+            this.dialogStat = true
+        }   
+    }
 }
 </script>
 <template>
     <v-container>
-        <vs-button gradient>+ Info</vs-button>
+        <vs-button gradient @click="onDialogShow">+ Info</vs-button>
+        <UniversalDialog
+        :dialogStat="dialogStat" 
+        :type="type"
+        :header="header"
+        @close="dialogStat = false"
+        />
     </v-container>
 </template>
