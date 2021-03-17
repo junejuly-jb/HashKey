eaaeea1219a6c8dcfaaf393b417c299e248b6a83
june <junearagoncillo@gmail.com>
2021-03-17 16:22:00 +0800

    + new modules for passwords

---
+++ client/src/store/Modules/password.js
@@ -0,0 +1,25 @@
+import HashKeyServices from '../../services/HashKeyServices'
+export const namespaced = true
+
+export const state = {
+    passwords: []
+}
+
+export const mutations = {
+    SET_PASSWORD(state, payload) {
+        state.passwords.push(payload)
+   }
+}
+
+export const actions = {
+    addPassword({ commit }, payload) {
+        return HashKeyServices.addPassword(payload)
+            .then(res => {
+                commit('SET_PASSWORD', res.data.data)
+                return res.status
+            })
+    }
+}
+
+export const getters = {
+}
