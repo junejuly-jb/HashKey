2ae5598819f8d77c3443f1aaed948f38ae191f16
june <junearagoncillo@gmail.com>
2021-03-31 14:22:54 +0800

    screenshot

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -4,4 +4,21 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                custom_red: "#F28B82",
+                custom_orange: "#FBBC04",
+                custom_yellow: "#FBBC04",
+                custom_green: "#CCFF90",
+                custom_teal: "#A7FFEB",
+                custom_blue: "#CBF0F8",
+                custom_darkblue: "#AECBFA",
+                custom_purple: "#D7AEFB",
+                custom_pink: "#FDCFE8",
+                custom_brown: "#E6C9A8",
+                custom_gray: "#E8EAED"
+            }
+        }
+    }
 });
