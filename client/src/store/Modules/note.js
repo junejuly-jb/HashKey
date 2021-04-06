227b92cffb0947637736ae4fd20d715be774ab70
june <junearagoncillo@gmail.com>
2021-04-06 16:54:51 +0800

    delete front end

--- client/src/store/Modules/note.js
+++ client/src/store/Modules/note.js
@@ -12,6 +12,12 @@ export const mutations = {
     FETCH_NOTES(state, payload) {
         state.notes = payload.filter(id => { return id.note_id != state.notes.note_id })
     },
+    DELETE_NOTES(state, payload) {
+        for (let i = 0; i < payload.length; i++) {
+            var idx = state.notes.findIndex(index => index.note_id === payload[i]);
+            state.notes.splice(idx, 1)
+        }
+    },
 }
 
 export const actions = {
@@ -35,7 +41,16 @@ export const actions = {
             return err.response.status
         })
     },
-    
+    deleteNotes({commit}, payload) {
+        return HashKeyServices.deleteNote(payload)
+        .then(response => {
+            commit('DELETE_NOTES', payload)
+            return response.status
+        })
+        .catch(err => {
+            return err.response.status
+        })
+    },
 }
 
 export const getters = {
