8c2b22367ad512d1adfc48d102525ee627c2b16b
june <junearagoncillo@gmail.com>
2021-04-29 11:03:10 +0800

    togglers

--- client/src/components/Filters/Card.vue
+++ client/src/components/Filters/Card.vue
@@ -14,12 +14,13 @@ export default {
 
         // confirmation dialog
         dialogStats: false,
-        message: "Do you wish to delete this card?",
+        message: "",
         width: "400px",
-        header_delete: "Delete",
-        status: "delete_card",
+        header_delete: "",
+        status: "",
 
-        selected_card_to_delete: {}
+        selected_card_to_delete: {},
+        ids: []
     }),
     methods: {
         to_chunk(str){
@@ -36,6 +37,12 @@ export default {
             var last_digit =  card_no.substring(card_no.length - 4)
             return last_digit
         },
+
+        format_card(str){
+            let text = str.match(/.{1,4}/g)
+            return text.join(' ')
+        },
+
         format_date(date){
             var datePart = date.match(/\d+/g),
             year = datePart[0].substring(2),
@@ -45,12 +52,59 @@ export default {
         },
 
         btnDeleteCard(card){
+            this.message = "Do you wish to delete this card?"
+            this.header_delete = "Delete"
+            this.status = "delete_card"
             this.dialogStats=true
             this.selected_card_to_delete = card
         },
         
+        hide(index){
+            this.$store.commit('card/SELECTED_TOGGLE', index)
+        },
+
+        copyCardNumber(){
+            this.$vs.notification({
+                title: 'Success',
+                color: 'primary',
+                width: 'auto',
+                text: 'Card number copied successfully!',
+                position: 'top-right',
+            })
+        },
+
         onDelete(){
-            console.log('on delete')
+            this.ids.push(this.selected_card_to_delete.card_id)
+            this.$store.dispatch('card/deleteCards', this.ids)
+            .then( res => {
+                if(res === 401){
+                    this.dialogStats=true
+                    this.status = "unauthorize"
+                    this.header_delete = "Session Expired"
+                    this.message="Session is expired please login to continue"
+                }
+                else if(res === 200){
+                    
+                    this.dialogStats = false
+                    this.$vs.notification({
+                        title: 'Success',
+                        color: 'success',
+                        width: 'auto',
+                        text: 'Card has been deleted successfully',
+                        position: 'top-right',
+                    })
+                    
+                }
+                else{
+                    this.$vs.notification({
+                        title: 'Error',
+                        color: 'danger',
+                        width: 'auto',
+                        text: 'Something went wrong',
+                        position: 'top-right',
+                    })
+                }
+            })
         }
     }
 }
@@ -66,13 +120,28 @@ export default {
                 <v-card elevation="0" v-for="(card, index) in cards" :key="index" :color="card.card_color" class="neo rounded-lg mb-5">
                     <v-container>
                         <div class="text-right">
-                            <v-btn @click="btnDeleteCard(card)" icon color="white" x-small>
+                            <span class="mr-2">
+                                <v-btn icon color="white" small @click="hide(index)">
+                                    <v-icon v-if="!card.card_selected">mdi-eye-off-outline</v-icon>
+                                    <v-icon v-else>mdi-eye-outline</v-icon>
+                                </v-btn>
+                            </span>
+                            
+                            <v-btn @click="btnDeleteCard(card)" icon color="white" small>
                                 <v-icon>mdi-close</v-icon>
                             </v-btn>
                         </div>
                         <div class="mt-8 px-5">
-                            <div class="pb-5">
-                                <pre class="white--text">{{to_chunk(card.card_number)}} {{get_last_digits(card.card_number)}}</pre>
+                            <div class="pb-5 d-flex">
+                                <pre class="white--text" v-if="!card.card_selected">{{to_chunk(card.card_number)}} {{get_last_digits(card.card_number)}} </pre>
+                                
+                                <pre class="white--text" v-else>{{format_card(card.card_number)}}</pre>
+                                <vs-tooltip>
+                                    <v-btn icon color="white" x-small v-clipboard:copy="card.card_number" @click="copyCardNumber">
+                                        <v-icon>mdi-content-copy</v-icon>
+                                    </v-btn>
+                                    <template #tooltip>Copy</template>
+                                </vs-tooltip>
                             </div>
                             <div class="my-3">
                                 <pre class="white--text">{{format_date(card.card_expiry)}}</pre>
