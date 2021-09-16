ce87c91ed9ad6b7dfba2c37321686d52b994c520
june <junearagoncillo@gmail.com>
2021-09-16 14:45:04 +0800

    added load fetching

--- client/src/components/Filters/Passwords.vue
+++ client/src/components/Filters/Passwords.vue
@@ -4,7 +4,7 @@ import InformationDialog from '../Main/InformationDialog'
 import ExportDialog from '../Main/ExportDialog.vue'
 import { mapState } from 'vuex'
 export default {
-    computed: { ...mapState('password', ['passwords']) },
+    computed: { ...mapState('password', ['passwords','pass_isLoading']) },
     components: { UniversalDialog, InformationDialog, ExportDialog },
     data: () => ({
         dialogStat: false,
@@ -38,26 +38,35 @@ export default {
             <vs-button gradient @click="dialogStat = true">+ Password</vs-button>
             <vs-button gradient @click="exportPass">Export</vs-button>
         </div>
-        <div class="mx-2" v-if="passwords.length > 0">
-            <masonry
-            :cols="{default: 3, 1000: 2, 700: 1, 400: 1}"
-            :gutter="{default: '30px', 700: '10px'}"
-            >
-                <div v-for="(pass, i) in passwords" :key="i" class="neo" @click="view(pass)">
-                    <v-container>
-                        <div class="text-center">
-                            <img :src="getImage(pass.log_website)" alt="" width="60" class="web_icons">
-                            <h4 class="my-2">{{pass.log_name}}</h4>
-                            <small>{{pass.log_email}}</small>
-                        </div>
-                    </v-container>
-                </div>
-            </masonry>
+        <div class="progress__bar__container" v-if="pass_isLoading">
+            <v-progress-circular
+            :size="50"
+            color="primary"
+            indeterminate
+            ></v-progress-circular>
         </div>
-        <div v-else class="mx-2 no_content_found">
-            <div class="text-center">
-                <h1>No login credentials to show</h1>
-                <pre>Click the <v-icon color="blue darken-2">mdi-lock-plus</v-icon> to add new credentials</pre>
+        <div v-else>
+            <div class="mx-2" v-if="passwords.length > 0">
+                <masonry
+                :cols="{default: 3, 1000: 2, 700: 1, 400: 1}"
+                :gutter="{default: '30px', 700: '10px'}"
+                >
+                    <div v-for="(pass, i) in passwords" :key="i" class="neo" @click="view(pass)">
+                        <v-container>
+                            <div class="text-center">
+                                <img :src="getImage(pass.log_website)" alt="" width="60" class="web_icons">
+                                <h4 class="my-2">{{pass.log_name}}</h4>
+                                <small>{{pass.log_email}}</small>
+                            </div>
+                        </v-container>
+                    </div>
+                </masonry>
+            </div>
+            <div v-else class="mx-2 no_content_found">
+                <div class="text-center">
+                    <h1>No login credentials to show</h1>
+                    <pre>Click the <v-icon color="blue darken-2">mdi-lock-plus</v-icon> to add new credentials</pre>
+                </div>
             </div>
         </div>
         <UniversalDialog 
@@ -101,5 +110,11 @@ export default {
     }
     .no_content_found > div > h1 {
         color: rgb(87, 87, 87);
+    }
+    .progress__bar__container{
+        display: flex;
+        height: 50vh;
+        align-items: center;
+        justify-content: center;
     }
 </style>
