6ef70a2195c06fe2152f1319a3b9e1df496b006e
june <junearagoncillo@gmail.com>
2021-09-24 14:36:35 +0800

    added contact list dropdown

--- client/src/components/Filters/InfoComponents/Contacts.vue
+++ client/src/components/Filters/InfoComponents/Contacts.vue
@@ -8,12 +8,40 @@
                     </v-avatar>
                 </v-col>
                 <v-col class="d-flex align-center py-3">
-                    <div>{{ contact.contact_fname}} {{ contact.contact_lname}}</div>
+                    <div>
+                        <div>{{ contact.contact_fname}} {{ contact.contact_lname}}</div>
+                        <small>{{ contact.contact_number }}</small>
+                    </div>
                 </v-col>
                 <v-col lg="1" md="1" class="d-flex py-3 justify-center align-center">
-                    <v-btn icon>
-                        <v-icon>mdi-dots-horizontal</v-icon>
-                    </v-btn>
+                    <v-menu offset-y :rounded="rounded-lg">
+                        <template v-slot:activator="{ on: menu, attrs }">
+                            <v-tooltip bottom>
+                            <template v-slot:activator="{ on: tooltip }">
+                                <v-btn
+                                icon
+                                v-bind="attrs"
+                                v-on="{ ...tooltip, ...menu }"
+                                >
+                                    <v-icon>mdi-dots-horizontal</v-icon>
+                                </v-btn>
+                            </template>
+                            <span>more</span>
+                            </v-tooltip>
+                        </template>
+                        <v-list dense>
+                            <v-list-item
+                            v-for="(item, index) in items"
+                            :key="index"
+                            link
+                            >
+                                <v-list-item-icon>
+                                    <v-icon v-text="item.icon"></v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-title>{{ item.title }}</v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
                 </v-col>
             </v-row>
         </div>
@@ -23,7 +51,10 @@
 import { mapState } from 'vuex'
 export default {
     data: () => ({
-
+        items: [
+        { title: 'Edit', icon: 'mdi-pencil-outline'},
+        { title: 'Delete', icon: 'mdi-delete-outline' },
+      ],
     }),
     computed: {
         ...mapState('contact', ['contacts'])
