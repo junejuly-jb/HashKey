51ae0952eb6d07cf948f708ec34f1897ea3d79f8
june <junearagoncillo@gmail.com>
2021-10-14 13:41:03 +0800

    added DL Model

---
+++ server/models/License.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const DLicenseSchema = new Schema({
+    credential_type: {
+        type: String,
+        required: true,
+        default: 'contact'
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    credentials: {
+        name: {
+            type: String
+        },
+        number: {
+            type: String
+        },
+        date_issued: {
+            type: Date
+        },
+        exp_date: {
+            type: String
+        }
+    }
+})
+
+module.exports = mongoose.model('DLicense', DLicenseSchema)
