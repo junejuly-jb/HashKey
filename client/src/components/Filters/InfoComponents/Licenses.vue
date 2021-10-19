2f1eae67b49c9b95775efccc9afe8a40891ed953
june <junearagoncillo@gmail.com>
2021-10-19 13:11:16 +0800

    features

--- client/src/components/Filters/InfoComponents/Licenses.vue
+++ client/src/components/Filters/InfoComponents/Licenses.vue
@@ -1,14 +1,36 @@
 <template>
+    <v-container>
     <ConfirmationDialog 
     :dialogStats="dialogStats"
     :message="message"
     :header="header"
     :status="status"
     />
+        <div class="mx-2" v-if="licenses.length > 0">
+                <masonry
+                :cols="{default: 3, 1000: 2, 700: 1, 400: 1}"
+                :gutter="{default: '30px', 700: '10px'}"
+                >
+                    <v-card elevation="0" v-for="(license, index) in licenses" :key="index" class="neo rounded-lg">
+                        <h1>Hello</h1>
+                    </v-card>
+                </masonry>
+            </div>
+            <div v-else class="mx-2 no_content_found">
+                <div class="text-center">
+                    <h1>No notes</h1>
+                    <pre>Click the <v-icon color="blue darken-2">mdi-note-plus</v-icon> to add new note.</pre>
+                </div>
+            </div>
+    </v-container>
 </template>
 <script>
 import ConfirmationDialog from '../../Main/ConfirmationDialog'
+import { mapState } from 'vuex'
 export default {
+    computed: { 
+        ...mapState('license', ['licenses']) 
+    },
     components: { ConfirmationDialog },
     data: () => ({
         dialogStats: false,
@@ -30,3 +52,15 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+    .no_content_found{
+        display: flex;
+        height: 50vh;
+        align-items: center;
+        justify-content: center;
+    }
+    .no_content_found > div > h1 {
+        color: rgb(87, 87, 87);
+    }
+</style>
